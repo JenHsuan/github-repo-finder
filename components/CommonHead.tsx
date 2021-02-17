ff2610a369e9aeafb927187695c813745166bc8a
JenHsuan <ofalpha@gmail.com>
2021-02-17 21:00:17 +0800

    Add the header

---
+++ components/CommonHead.tsx
@@ -0,0 +1,43 @@
+import React, {useEffect} from 'react'
+import Head from 'next/head';
+import PropTypes from 'prop-types'
+
+type CommandHeadProp = {
+    title: string;
+    url: string;
+    description: string;
+    keywords: string;
+}
+
+const CommonHead: React.FC<CommandHeadProp> = ({title, url, description, keywords}) => {
+    return (
+        <Head>
+            <title>{title}</title>
+            <link rel="icon" href="https://raw.githubusercontent.com/JenHsuan/ALayman/master/views/images/alaymanicon.png" type="image/x-icon" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta property="og:title" content={title} />
+            <meta property="og:url" content={url}/>
+            <meta property="og:image" content="https://raw.githubusercontent.com/JenHsuan/github-repo-finder/master/screen-shot/cover.png"/>
+            <meta property="og:description" content={description}/>
+            <meta name="keywords" content={keywords}/>
+            <meta name="author" content="Jen-Hsuan Hsieh (Sean Hsieh)"/>
+            <link rel="dns-fetch" href="https://raw.githubusercontent.com/"/>
+            <link rel="canonical" href={url}/>
+            <link rel="manifest" href="/_next/static/manifest.webmanifest" />
+            <meta name = "apple-mobile-web-app-capable" content="yes" />
+            <meta name = "apple-mobile-web-app-status-bar-style" content="black" />
+            <meta name = "apple-mobile-web-app-title" content={title} />
+            <link rel="apple-touch-icon" href="/_next/static/logo-icon-144x144.png" />
+            <link rel="apple-touch-startup-icon" href="/_next/static/logo-icon-144x144.png" />
+        </Head>
+    )
+}
+
+CommonHead.propTypes ={
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    keywords: PropTypes.string.isRequired
+}
+
+export default CommonHead
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import Head from 'next/head';
 import Dashboard from '../components/Dashboard';
 import 'intersection-observer'
+import CommonHead from '../components/CommonHead';
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Home</title>
-      </Head>
+      <CommonHead title = "ALayman Gihub-Repo-Finder"
+                  url = 'https://next-github-repo-finder.netlify.app/'
+                  description = 'Project for searching repositories with keywords. There is only one page with a single searching function.'
+                  keywords = 'next, redux, github, nefify'>
+      </CommonHead>
       <Dashboard/>
     </div>
   )
