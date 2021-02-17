fcb8111dbcfbcf8175c22d3ac444bfcd09599004
JenHsuan <ofalpha@gmail.com>
2021-02-17 21:15:08 +0800

    remove useless code

--- components/CommonHead.tsx
+++ components/CommonHead.tsx
@@ -13,16 +13,10 @@ const CommonHead: React.FC<CommandHeadProp> = ({title, url, description, keyword
     return (
         <Head>
             <title>{title}</title>
-            <link rel="icon" href="https://raw.githubusercontent.com/JenHsuan/ALayman/master/views/images/alaymanicon.png" type="image/x-icon" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <meta property="og:title" content={title} />
-            <meta property="og:url" content={url}/>
             <meta property="og:image" content="https://raw.githubusercontent.com/JenHsuan/github-repo-finder/master/screen-shot/cover.png"/>
-            <meta property="og:description" content={description}/>
             <meta name="keywords" content={keywords}/>
             <meta name="author" content="Jen-Hsuan Hsieh (Sean Hsieh)"/>
-            <link rel="dns-fetch" href="https://raw.githubusercontent.com/"/>
-            <link rel="canonical" href={url}/>
         </Head>
     )
 }
